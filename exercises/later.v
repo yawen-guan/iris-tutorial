e7fc66c7ce1c116f3b34c5d9e2b98e9e8c8afc46
Yawen Guan <yawen.guan@proton.me>
2026-03-18 11:57:08 +0100

    exercise: Finish later

--- exercises/later.v
+++ exercises/later.v
@@ -93,8 +93,10 @@ Qed.
 
 Lemma later_impl (P Q : iProp Σ) : P ∗ ▷ (P -∗ Q) -∗ ▷ Q.
 Proof.
-  (* exercise *)
-Admitted.
+  iIntros "[HP HPQ]".
+  iNext.
+  iApply "HPQ". by iFrame.
+Qed.
 
 (* ================================================================= *)
 (** ** Tying Later to Program Steps *)
